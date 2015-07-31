4ca0f9e4fb28e733723419c734a81707bfcd693f
Gabriel Cousin <csngab@gmail.com>
2015-07-31 10:02:37 +0200

    Remove sourcemaps - fix #117

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,14 +1,11 @@
 var sass       = require('gulp-sass');
 var prefix     = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function () {
   return gulp.src('./src/scss/**/*.scss')
-		.pipe(sourcemaps.init())
 		.pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix({
       browsers: ['last 2 versions']
     }))
-		.pipe(sourcemaps.write())
     .pipe(gulp.dest('./dist'));
 };
