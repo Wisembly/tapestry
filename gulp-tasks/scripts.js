eb9a4d8b2c4c092479b6ffac4d2d0fc7fbac1d24
Gabriel Cousin <csngab@gmail.com>
2015-08-27 17:31:07 +0200

    JS | update Gulp task

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -2,9 +2,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 module.exports = function () {
-  return gulp.src('./src/js/*.js', { debug: true })
+  return gulp.src('./src/js/*.js')
     // .pipe(uglify())
-    .pipe(gulp.dest('dist'))
     .pipe(concat('tapestry.js'))
     .pipe(gulp.dest('dist'));
 };
