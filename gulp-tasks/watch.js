c30f27d50bddf8bf914773fc10596aee73baedea
onaliugo <hello@onali-ugo.fr>
2015-09-08 16:52:15 +0200

    NW | Inject Styleguide CSS

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -4,6 +4,9 @@ var bs = require('browser-sync');
 module.exports = function () {
   gulp.watch('website/views/**', ['bs:reload']);
   gulp.watch('src/scss/**', ['sass']);
-  return gulp.watch('src/js/**', ['scripts']);
+  gulp.watch('src/js/**', ['scripts']);
+  return gulp.watch('website/public/css/**', bs.reload({
+    stream: true
+  }));
   // gulp.watch(['./src/**/*.md'], ['styleguide']);
 };
