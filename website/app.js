4897729c81c41136a93c907376af28894e36ebc8
onaliugo <hello@onali-ugo.fr>
2015-09-09 11:26:40 +0200

    NW | Add other pages, update script block

--- website/app.js
+++ website/app.js
@@ -1,6 +1,22 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var pages = [{
+  hash: '/',
+  template: 'index'
+}, {
+  hash: '/about',
+  template: 'about'
+}, {
+  hash: '/components',
+  template: 'components'
+}, {
+  hash: '/getting-started',
+  template: 'getting-started'
+}, {
+  hash: '*',
+  template: '404'
+}];
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade');
@@ -10,18 +26,10 @@ app.use('/', router);
 
 app.listen(3000);
 
-router.get('/', function (req, res) {
-  res.render('index', {
-    currentPage: 'home'
+pages.forEach(function (page) {
+  router.get(page.hash, function (req, res) {
+    res.render(page.template, {
+      currentPage: page.template
+    })
   });
-});
-
-router.get('/about', function (req, res) {
-  res.render('about', {
-    currentPage: 'about'
-  });
-});
-
-router.get('*', function(req, res){
-  res.render(404);
 });
