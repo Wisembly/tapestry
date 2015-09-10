a910a19b13b46b2a7953b8468c8b721d37f2aa6e
onaliugo <hello@onali-ugo.fr>
2015-09-10 14:34:18 +0200

    NW | Fix Sass task, re-add Styleguide one

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -3,10 +3,10 @@ var bs = require('browser-sync');
 
 module.exports = function () {
   gulp.watch('website/views/**', ['bs:reload']);
-  gulp.watch('src/scss/**', ['sass']);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
   gulp.watch('src/js/**', ['scripts']);
+  gulp.watch('src/**/*.md', ['styleguide', 'bs:reload']);
   return gulp.watch('website/public/css/**', bs.reload({
     stream: true
   }));
-  // gulp.watch(['./src/**/*.md'], ['styleguide']);
 };
