617b4b6869734f7ed8d3343325e925ca18536052
onaliugo <hello@onali-ugo.fr>
2015-09-11 14:42:25 +0200

    NW | Fix components route

--- website/routes/components.js
+++ website/routes/components.js
@@ -4,13 +4,14 @@ var components = require('../components');
 module.exports = function (app) {
   app.get('/components/:name?', function (req, res) {
     var name = req.params.name;
+
     var component = name ? _.findWhere(components, {
       name: _(name).capitalize()
     }) : null;
 
     return res.render(name ? 'component' : 'components', {
       currentPage: 'components',
-      currentComponent: component.name,
+      currentComponent: component ? component.name : null,
       components: components,
       component: component
     });
