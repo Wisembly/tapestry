7981bfd8fd587b4c2d25f84d6d326fe262e79985
onaliugo <hello@onali-ugo.fr>
2015-09-17 18:25:23 +0200

    Stats | Read dist/stats and give it to the app

--- website/routes/stats.js
+++ website/routes/stats.js
@@ -1,7 +1,18 @@
+var utils = require('uo-node-utils');
+var path = require('path');
+
 module.exports = function (app) {
-  app.get('/stats', function (req, res) {
+  var stats = [];
+  var paths = utils.read.folder(path.join(__dirname, '../../dist/stats'), 'utf8');
+
+  paths.forEach(function (path) {
+    stats.push(utils.read.file(path));
+  });
+
+  return app.get('/stats', function (req, res) {
     res.render('stats', {
-      currentPage: 'stats'
+      currentPage: 'stats',
+      stats: stats
     });
   });
 };
