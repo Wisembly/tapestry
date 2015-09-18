7dd31e2b0c2674ad0159e11dcaf2d1fd58827fb6
onaliugo <hello@onali-ugo.fr>
2015-09-18 11:34:54 +0200

    Stats | Add example w/ filesize

--- website/public/js/stats.js
+++ website/public/js/stats.js
@@ -1,6 +1,32 @@
 $(function () {
 
   $.get('/stats/json', function (data) {
-    console.log(data);
-  })
+    data = data.map(function (el) {
+      return el.size;
+    });
+
+    initChart(data);
+  });
+
+  function initChart(_data) {
+    var data = {
+      labels: ["0.20", "0.30"],
+      datasets: [
+        {
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          // data: [28, 48, 40, 19, 86, 27, 90]
+          data: _data
+        }
+      ]
+    };
+    var ctx = document.getElementById('js-Stats').getContext('2d');
+    var myLineChart = new Chart(ctx).Line(data);
+  }
+
 });
