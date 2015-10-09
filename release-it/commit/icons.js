75bba0cd5c2dee3d0da27645804ad7a6dbc3d3ae
onaliugo <hello@onali-ugo.fr>
2015-10-09 16:46:52 +0200

    Release-it | Ignore and publish on release css/js/icons

--- release-it/commit/icons.js
+++ release-it/commit/icons.js
@@ -3,13 +3,13 @@ var opts = require('../opts');
 
 module.exports = {
   generate: function (cb) {
-    exec('gulp sass', function (err, data) {
+    exec('gulp icons', function (err, data) {
       if (err) return false;
       return cb(null);
     });
   },
   add: function (cb) {
-    exec('git add -f dist/css/tapestry.css', function (err, data) {
+    exec('git add -f dist/fonts/', function (err, data) {
       if (err) return false;
       return cb(null);
     });
