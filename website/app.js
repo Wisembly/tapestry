372edfa51163d9619b88e646b38f6f748881b6dd
Gabriel Cousin <csngab@gmail.com>
2017-04-03 16:43:34 +0200

    Styleguide | Move rename pugfiles and move assets

--- website/app.js
+++ website/app.js
@@ -9,14 +9,13 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'pug');
 
 app.use(compression());
-app.use(express.static(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 require('./routes/index')(app);
 require('./routes/getting-started')(app);
 require('./routes/components')(app);
 require('./routes/sass-utils')(app);
-require('./routes/stats')(app);
+require('./routes/statistics')(app);
 require('./routes/404')(app);
 
 app.listen(3000);
