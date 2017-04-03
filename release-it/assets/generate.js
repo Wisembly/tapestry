4c310713eb81dd19f8201073f5d91c03ec48ced2
Gabriel Cousin <csngab@gmail.com>
2017-04-03 16:47:37 +0200

    Release-it | Generate static doc files

--- release-it/assets/generate.js
+++ release-it/assets/generate.js
@@ -2,9 +2,18 @@ var async = require('async');
 var exec = require('../tools/exec');
 var log = require('../tools/log');
 var opts = require('../opts');
+var pug = require('pug');
+var utils = require('uo-node-utils');
 
 module.exports = function (cb) {
   async.waterfall([
+    function (_cb) {
+      exec('rm -rf dist/**/*.html temp', function (err, data) {
+        if (err) return false;
+        return _cb(null);
+      });
+    },
+
     function (_cb) {
       exec('gulp icons', function (err, data) {
         if (err) return false;
@@ -28,6 +37,48 @@ module.exports = function (cb) {
 
     function (_cb) {
       exec('gulp scripts', function (err, data) {
+        if (err) return false;
+        return _cb(null);
+      });
+    },
+
+    function (_cb) {
+      var stats = require('../../dist/stats/' + opts.nextRelease.name + '.json');
+      stats.version = opts.nextRelease.name
+
+      utils.create.file('temp/statsOptions.json', JSON.stringify({ stats: [stats] }))
+
+      exec('pug website/views/statistics.pug -O temp/statsOptions.json --out dist', function (err, data) {
+        if (err) return false;
+        return _cb(null);
+      });
+    },
+
+    function (_cb) {
+      var components = require('../../website/components/components.json').components;
+
+      components.forEach(function (component) {
+        var filename = component.name.toLowerCase();
+        utils.create.file('temp/' + filename + '.json', JSON.stringify({
+          components: components,
+          component: component,
+          version: opts.nextRelease.name,
+        }));
+
+        var html = pug.renderFile('website/views/components.pug', {
+          components: components,
+          component: component,
+          version: opts.nextRelease.name,
+        })
+
+        utils.create.file('dist/components/' + filename + '.html', html);
+      })
+
+      return _cb(null)
+    },
+
+    function (_cb) {
+      exec('pug website/views/{index,404,sass-utils,getting-started}.pug --out dist', function (err, data) {
         if (err) return false;
         return _cb(null);
       });
